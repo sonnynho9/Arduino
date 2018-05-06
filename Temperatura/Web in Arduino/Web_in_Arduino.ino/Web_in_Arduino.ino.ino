6c44783808340a2311f12646cf9ca11c9c7f897d
Edival Gonçalves Gomes <edival.gomes91@live.com>
2018-05-06 20:36:56 -0300

    Web

--- Temperatura/Web in Arduino/Web_in_Arduino.ino/Web_in_Arduino.ino.ino
+++ Temperatura/Web in Arduino/Web_in_Arduino.ino/Web_in_Arduino.ino.ino
@@ -7,9 +7,9 @@
 #include <Ethernet.h> //Biblioteca do Ethernet 
 #include <SPI.h> //Biblioteca de Comunicação Ethernet e Arduino
 
-#define DHTPIN A1 //Pino do Sensor DHT11 na porta Analógica 01
+// #define DHTPIN A1 //Pino do Sensor DHT11 na porta Analógica 01
 #define DHTTYPE DHT11 //Definindo o tipo de sensor utilizado DHT11
-DHT dht(DHTPIN, DHTTYPE);
+//DHT dht(DHTPIN, DHTTYPE);
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -26,7 +26,7 @@ EthernetServer server(80); //Inicializa o servidor web na porta 80
 void setup() {
   Ethernet.begin(mac, ip, gateway, subnet); //Inicializa a interface de rede
   server.begin(); //Inicializa o Servidor Web
-  dht.begin();
+//  dht.begin();
   lcd.begin(16, 2); //Escolhendo o numero de colunas e linha no LCD
   lcd.print("TestDHT"); //Teste do DHT
 
@@ -34,19 +34,20 @@ void setup() {
 }
 
 void loop() {
-
-  //Aguardando conexão do Browser
-  EthernetClient client = server.available();
-  if (client) 
-  {
-    boolean currentLineIsBlank = true;
-    while (client.connected())
-    {
-      if (client.available())
-      {
-        char c = client.read();
-        if (client == '\n' && currentLineIsBlank);
-      }
+    EthernetClient client = server.available();  
+ 
+    if (client) 
+    { 
+      
+        boolean currentLineIsBlank = true;
+        while (client.connected()) 
+        {
+            if (client.available())
+            {   
+                char c = client.read(); 
+                
+                if (c == '\n' && currentLineIsBlank) 
+                {
         //Aqui começa a página web
         client.println("HTTP/1.1 200 0K");
         client.println("Content-Type: text/css");
@@ -57,10 +58,26 @@ void loop() {
         client.println(" <html> ");
         client.println("<head>");
         client.println("<Title>Controlando a temperatura</title>");
+        client.println("<link rel='stylesheet' type='text/css' href='https://cdn.rawgit.com/sonnynho9/Arduino/Temperatura/Temperatura/style.css' />");
         client.println("</head)");
-        client.println("body { background-color: #00ffff;");
+        client.println("<body>");
+        client.println("<body bgcolor='#00FFFF'/>");
+        client.println("</body>");
+        client.println("</html>");
+}
+                
+                if (c == '\n') 
+                {
+                    currentLineIsBlank = true;
+                } 
+                else if (c != '\r') 
+                {
+                    currentLineIsBlank = false;
+                }
+            } 
+        }
+        
 
       }
     }
-  }
 
